19fbbc8ae4e223c7ffe0f24e6db621e736aea20f
pkbceira03 <cabeceira2003@gmail.com>
2023-05-29 19:12:58 -0300

    goglemaps com js

---
+++ src/js/function.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    var map;
+    function initialize(){
+        var mapProp = {
+            center: new google.maps.LatLng(-27.648598,-48.577423),
+            scrollWheel:false,
+            zoom:12,
+            MapTypeId:google.maps.MapTypeId.ROADMED
+        }
+        map = new google.maps.Map(document.getElementById("mapa"),mapProp);
+    }
+
+    initialize();
+}
